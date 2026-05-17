83ba04b7fbc7a445970de055f53965df296bd953
01brandon <brandonmukuria@gmail.com>
2026-05-18 02:21:03 +0300

    Commit 4: Create database models for CustomUser Photo Tag Like Dislike

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+import uuid
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True, null=True, max_length=500)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.username
+
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, validators=[MinLengthValidator(3)])
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='photos/')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='photos')
+    tags = models.ManyToManyField(Tag, related_name='photos', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def total_dislikes(self):
+        return self.dislikes.count()
+
+
+class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['photo', 'user']
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.photo.title}'
+
+
+class Dislike(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='dislikes')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['photo', 'user']
+
+    def __str__(self):
+        return f'{self.user.username} dislikes {self.photo.title}'
